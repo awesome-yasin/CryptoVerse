351e7e714f40bbcab1238dfe3e112fe4f3b473cd
MD Yasin <85479838+awesome-yasin@users.noreply.github.com>
2021-12-01 20:51:30 +0530

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+const path = require('path');
 var express = require("express");
+
 
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
+
+
+app.use(express.json({ extended: false }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
 
 app.use(express.static('public'));
 
@@ -10,6 +24,15 @@ app.use(express.static('public'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
  app.use('/images', express.static(__dirname + '/public/images'));
+
+  app.use(express.static(path.join(__dirname, "/client", "build")));
+
+  app.route('/portfolio')
+  .get((req,res)=>{
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
+
+
 
  app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
